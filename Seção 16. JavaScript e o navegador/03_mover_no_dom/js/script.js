f38a624ba0e30ea47591be97728ac513731dc3a9
Laercio Pereira <laercio3714@gmail.com>
2024-11-23 17:21:41 -0300

    exercicios js

---
+++ Seção 16. JavaScript e o navegador/03_mover_no_dom/js/script.js
@@ -0,0 +1,5 @@
+console.log(document.body);
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1].childNodes[3].innerText);
+console.log(document.body.childNodes[3].childNodes);
