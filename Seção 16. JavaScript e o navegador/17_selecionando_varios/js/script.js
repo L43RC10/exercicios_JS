b77471065ed82e9d648d42badc8c6794f6859d06
Laercio Pereira <laercio3714@gmail.com>
2024-12-25 11:48:58 -0300

    exercicios js

---
+++ Seção 16. JavaScript e o navegador/17_selecionando_varios/js/script.js
@@ -0,0 +1,9 @@
+let itens = document.querySelectorAll('.itens-vermelhos');
+console.log(itens[0].style.color);
+
+itens[0].style.color = 'green';
+
+console.log(itens[0].style.color);
+
+let itens2 = document.querySelectorAll('.itens-azuis');
+console.log(itens2);
