1c7c4cc57ee9b7793917c7ed652a975c82e12d54
Laercio Pereira <laercio3714@gmail.com>
2025-01-14 22:18:20 -0300

    exercicios js

--- Seção 17 Eventos com JavaScript/07.mais sobre mouse/script.js
+++ Seção 17 Eventos com JavaScript/07.mais sobre mouse/script.js
@@ -1,5 +1,19 @@
-let btn1 = document. querySelector("#btn01");
+let btn1 = document.querySelector("#btn01");
+let btn2 = document.querySelector("#btn02");
 
-btn1.addEventListener('mousedown', function(){
+btn1.addEventListener("mousedown", function(){
     console.log('apertou o botão');
-})
+});
+
+btn1.addEventListener("mouseup", function(){
+    console.log('soltou o botão');
+});
+
+btn2.addEventListener("dblclick", function(){
+    console.log('clicou duas vezes');
+});
+
+btn2.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+    console.log('clicou duas vezes btn direito');
+});
