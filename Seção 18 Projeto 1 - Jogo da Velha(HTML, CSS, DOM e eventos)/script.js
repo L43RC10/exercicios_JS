6aef9833d10ee22d179128ae1c0565454f1b2f63
Laercio Pereira <laercio3714@gmail.com>
2025-04-28 20:23:55 -0300

    exercicios js

--- Seção 18 Projeto 1 - Jogo da Velha(HTML, CSS, DOM e eventos)/script.js
+++ Seção 18 Projeto 1 - Jogo da Velha(HTML, CSS, DOM e eventos)/script.js
@@ -59,7 +59,23 @@ function checkElemento(player1, player2){
 
 // Ver quem ganhou
 function checkWinCondition(){
-    
+    let b1 = document.getElementById("block-1");
+    let b2 = document.getElementById("block-2");
+    let b3 = document.getElementById("block-3");
+    let b4 = document.getElementById("block-4");
+    let b5 = document.getElementById("block-5");
+    let b6 = document.getElementById("block-6");
+    let b7 = document.getElementById("block-7");
+    let b8 = document.getElementById("block-8");
+    let b9 = document.getElementById("block-9");
+
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0){
+
+        let b1Child = b1.childNodes[0].className;
+        let b2Child = b2.childNodes[0].className;
+        let b3Child = b3.childNodes[0].className;
+
+    }
 }
 
 // Quer levar um soco na cara?
