3f115d9f80774614a6c16b753f3765f03494df14
l43rc10 <laercio3714@gmail.com>
2025-06-08 16:40:20 -0300

    iniciando estruturação do JS

--- Seção 19 Aprendendo Node.js/328.express/index.js
+++ Seção 19 Aprendendo Node.js/328.express/index.js
@@ -7,7 +7,11 @@ app.get('/', function(req, res) {
 
 app.get('/teste', function(req, res) {
     res.send('testando outra rota');
-})
+});
+
+app.get('/db', function(req, res) {
+    res.send('link para o Data Base');
+});
 
 app.listen(3000, function(){
     console.log('A aplicação esta funcionando na porta 3000');
