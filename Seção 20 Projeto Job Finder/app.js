f12b972f7ab5df2fc74ab5156b7c4b1ec62a526b
Laercio Pereira <laercio3714@gmail.com>
2025-06-10 16:35:02 -0300

    exercicios js

--- Seção 20 Projeto Job Finder/app.js
+++ Seção 20 Projeto Job Finder/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
 
@@ -7,6 +8,18 @@ app.listen(PORT, function() {
     console.log(`O Express esta rodando na porta ${PORT}`)
 });
 
+
+// db connection
+db
+.authenticate()
+.then(() => {
+    console.log("Conectado com sucesso");
+})
+.catch(err => {
+    HTMLFormControlsCollection.log(`Ocorreu um erro ao conectar = ${err}`)
+});
+
+// rotes
 app.get('/', (req, res) => {
     res.send('Esta funcionando... 1');
 });
