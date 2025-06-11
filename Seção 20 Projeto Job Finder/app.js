cd311b3754ee77e4f510e6aaf6cc053d72e06b73
Laercio Pereira <laercio3714@gmail.com>
2025-06-11 20:22:26 -0300

    exercicios js

--- Seção 20 Projeto Job Finder/app.js
+++ Seção 20 Projeto Job Finder/app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
-const db = require('./db/connection');
+const express    = require('express');
+const app        = express();
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
@@ -8,6 +9,8 @@ app.listen(PORT, function() {
     console.log(`O Express esta rodando na porta ${PORT}`)
 });
 
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 db
@@ -23,3 +26,6 @@ db
 app.get('/', (req, res) => {
     res.send('Esta funcionando... 1');
 });
+
+// jobs routes
+app.use('/jobs', require('./routes/rotas_jobs'));
---
+++ Seção 20 Projeto Job Finder/routes/rotas_jobs.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router  = express.Router();
+const Job     = require('../models/Job');
+
+// rota de teste
+router.get('/teste', (req, res) => {
+    res.send('deu certo.. eu acho...');
+})
+
+// add job via post
+router.post('/add', (req, res) => {
+
+    let {title, salary, company, description, email, new_job} = req.body;
+
+// insert dados no sistema 
+Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+})
+.then(() => res.redirect('/'))
+.catch(err => console.log(err));
+
+});
+
+module.exports = router;
