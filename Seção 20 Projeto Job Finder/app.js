5af27cb0653195550cba8041eedbad659f04b47b
Laercio Pereira <laercio3714@gmail.com>
2025-06-21 21:04:07 -0300

    exercicios js

--- Seção 20 Projeto Job Finder/app.js
+++ Seção 20 Projeto Job Finder/app.js
@@ -1,5 +1,7 @@
 const express    = require('express');
+const exphbs     = require('express-handlebars')
 const app        = express();
+const path       = require('path');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
 
@@ -12,8 +14,13 @@ app.listen(PORT, function() {
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // db connection
-db
+db  
 .authenticate()
 .then(() => {
     console.log("Conectado com sucesso");
