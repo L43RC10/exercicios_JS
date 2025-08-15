aa31eeaf61ab9ccec1b30264475c97fc17acc9be
Laercio Pereira <laercio3714@gmail.com>
2025-08-14 21:13:44 -0300

    exercicios js

--- Seção 21 Projeto  3 Quizz com vanilla JS (Manipulação e evento de DOM)/js/script.js
+++ Seção 21 Projeto  3 Quizz com vanilla JS (Manipulação e evento de DOM)/js/script.js
@@ -1,1 +1,10 @@
-console.log('teste');
+// Declaração variaveis
+const question = document.querySelector('#question');
+const answersBox = document.querySelector('#answer-box');
+const quizzContainer = document.querySelector('quizz-container');
+const scoreContainer = document.querySelector('#score-container');
+const letters = ['a', 'b', 'c', 'd'];
+let points = 0;
+let actualQuestion = 0;
+
+// Perguntas
