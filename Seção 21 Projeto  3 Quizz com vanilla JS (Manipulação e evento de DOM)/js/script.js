62e4dd39ea242609a0d57a20b130d6dbe1cd5b80
Laercio Pereira <laercio3714@gmail.com>
2025-08-15 17:44:54 -0300

    exercicios js

--- Seção 21 Projeto  3 Quizz com vanilla JS (Manipulação e evento de DOM)/js/script.js
+++ Seção 21 Projeto  3 Quizz com vanilla JS (Manipulação e evento de DOM)/js/script.js
@@ -8,6 +8,77 @@ let points = 0;
 let actualQuestion = 0;
 
 // Perguntas
+const questions = [
+    {
+      "question": "PHP foi desenvolvido para qual fim?",
+      "answers": [
+        {
+          "answer": "back-end",
+          "correct": true
+        },
+        {
+          "answer": "front-end",
+          "correct": false
+        },
+        {
+          "answer": "Sistema operacional",
+          "correct": false
+        },
+        {
+          "answer": "Banco de dados",
+          "correct": false
+        },
+      ]
+    },
+    {
+      "question": "Uma forma de declarar variável em JavaScript:",
+      "answers": [
+        {
+          "answer": "$var",
+          "correct": false
+        },
+        {
+          "answer": "var",
+          "correct": true
+        },
+        {
+          "answer": "@var",
+          "correct": false
+        },
+        {
+          "answer": "#let",
+          "correct": false
+        },
+      ]
+    },
+    {
+      "question": "Qual o seletor de id no CSS?",
+      "answers": [
+        {
+          "answer": "#",
+          "correct": true
+        },
+        {
+          "answer": ".",
+          "correct": false
+        },
+        {
+          "answer": "@",
+          "correct": false
+        },
+        {
+          "answer": "/",
+          "correct": false
+        },
+      ]
+    },
+  ]
 
+//   Substituição do quizz para a primeira pergunta
 
-// paramos na aula 370, na minutagem 3:30
+function init(){
+    // criar primeira pergunta
+    console.log('Inicial');
+}
+
+init();
